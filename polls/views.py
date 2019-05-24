c06b74656e172ccc4162d9e03f71efd8c9f4b493
Grzegorz Kuprianowicz <grzegorz.kuprianowicz@tomtom.com>
2019-05-24 14:40:19 +0200

    Add parametrized views.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse('Hello world. Wou are at polls index page.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
